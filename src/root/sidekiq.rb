ec675f6bf85998357296d8e607c22ce4867af13d
tomoya <ihatov08@gmail.com>
2017-10-20 11:40:35 +0900

    [add]sidekiq basic auth

--- src/root/sidekiq.rb
+++ src/root/sidekiq.rb
@@ -1,3 +1,5 @@
+require "sidekiq/web"
+
 url = { url: "#{ENV['REDIS_URL']}" }
 
 Sidekiq.configure_client do |config|
@@ -6,4 +8,8 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = url
+end
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  user == ENV.fetch('SIDEKIQ_USER') && password == ENV.fetch('SIDEKIQ_PASSWORD')
 end
